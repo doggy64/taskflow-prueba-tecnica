8f218f4845b9bf2f7420ebf5fe8e2d07d3f42b7f
doggy64 <rickyespanol8@gmail.com>
2026-03-16 11:44:43 -0400

    fix:correcion del login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,9 @@
-import RegisterForm from '@/components/auth/RegisterForm'
+import LoginForm from '@/components/auth/LoginForm'
 
-export default function RegisterPage() {
+export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <RegisterForm />
+      <LoginForm />
     </div>
   )
 }
